5bf9fe2003ca80adf7252a067167599d159eff6f
waruru1890 <erikawaruru1818@gmail.com>
2022-04-21 01:08:06 +0700

    Pertemuan Kedua Sampai Insert Data

---
+++ config/database.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql');
+const koneksi = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'dbmovie',
+  multipleStatements: true,
+});
+
+koneksi.connect((err) => {
+  if (err) throw err;
+  console.log("MySQL Connected");
+});
+module.exports = koneksi;
